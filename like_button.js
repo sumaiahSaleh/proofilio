9bf9bd4cc1e49d3d22ef5fb1b3b6e2e063760f5e
sumaiahSaleh <41887652+sumaiahSaleh@users.noreply.github.com>
2018-08-07 05:30:40 +0300

    Add files via upload

---
+++ like_button.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Thank you to like my porofiol.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+
+/*const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);*/
+
+
+
+
+
